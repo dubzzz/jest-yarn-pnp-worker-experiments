efc810b4e3f9e5fe0a1b3ddc26672d120691ca64
Nicolas DUBIEN <github@dubien.org>
2024-02-18 21:25:23 +0000

    esm works too

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-import { workerData } from "node:worker_threads";
+import { parentPort, workerData } from "node:worker_threads";
 import prand from "pure-rand";
 
 parentPort.postMessage({ computed: workerData.raw * workerData.raw });
